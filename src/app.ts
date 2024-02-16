893bd0b748b8ed3ddc3713cbca38de1b92b8fe21
LeeSeungjae00 <seungjae266789@gmail.com>
2024-02-16 20:48:30 +0900

    ✨ MA line 만드는 함수 구축

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { CronJob } from 'cron';
 import dotenv from 'dotenv';
 import logger from './loaders/logger';
+import { getAccount, getCandles, getMarkets } from './api/upbit';
+import { getMALine } from './util/maLine';
 
 dotenv.config();
 
@@ -15,3 +17,10 @@ dotenv.config();
 //   null,
 //   true
 // );
+(async () => {
+  const data = await getAccount();
+  const market = await getMarkets();
+  const btc = await getCandles({ count: 200, market: market[0].market });
+
+  console.log(getMALine(btc));
+})();
