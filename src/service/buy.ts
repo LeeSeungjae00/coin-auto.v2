3f8593ad5994b07d058afd5dff2561e7c59e815b
LeeSeungjae00 <seungjae266789@gmail.com>
2024-02-18 19:09:00 +0900

    🚑  구매 api 연속 보내기 취소

--- src/service/buy.ts
+++ src/service/buy.ts
@@ -2,11 +2,13 @@ import { postBuyCoin } from '../api/upbit';
 import { CoinNavigator } from '../interface/upbit';
 import logger from '../loaders/logger';
 import { slackSend } from '../utils/slack';
+import { sleep } from '../utils/sleep';
 
 export const buy = async (market: CoinNavigator[]) => {
   for (const coin of market.filter((val) => val.status === 'buy')) {
     await postBuyCoin(coin.market, '10000');
     logger.info(`${coin.market} | ${coin.korean_name} | 10000원 매수 완료`);
     slackSend(`${coin.market} | ${coin.korean_name} | 10000원 매수 완료`);
+    sleep(100);
   }
 };
