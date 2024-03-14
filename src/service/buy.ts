9b25e917c027af1181fba4c2945a6d3de1313ddb
LeeSeungjae00 <seungjae266789@gmail.com>
2024-03-14 22:18:58 +0900

    🐛 MAX_BUY_COUNT 변수 사용

--- src/service/buy.ts
+++ src/service/buy.ts
@@ -3,14 +3,14 @@ import { Account, CoinNavigator } from '../interface/upbit';
 import logger from '../loaders/logger';
 import { sleep } from '../utils/sleep';
 
-const MAX_BUY_COUNT = 80;
+const MAX_BUY_COUNT = 85;
 
 export const buy = async (market: CoinNavigator[], account: Account[]) => {
   const totalcapital = account.reduce((prev, curr) => {
     if (curr.currency === 'KRW') return (prev += Number(curr.balance));
     return (prev += Number(curr.balance) * Number(curr.avg_buy_price));
   }, 0);
-  const price = totalcapital / 85;
+  const price = totalcapital / MAX_BUY_COUNT;
 
   for (const coin of market.filter((val) => val.status === 'buy')) {
     await postBuyCoin(coin.market, price.toString());
